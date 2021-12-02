174b3ca6222d6994ccb407321c73da39e64fb2b6
somei-T <somei_tanoue@jp.honda>
2021-12-02 19:40:18 +0900

    ✨素体の完成！！

---
+++ Tools/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Tools.ValueObject;
+using Tools.ValueObject.DateAndTime;
+using Tools.ValueObject.SizeObj;
+using Tools.Support.LinqLike;
+
+/*
+namespace Tools
+{
+    /// <summary> エントリポイント </summary>
+    public class Program
+    {
+        /// <summary> エントリポイント </summary>
+        /// <param name="args">引数</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!くそったれ！！！！！！");
+
+            return;
+        }
+    }
+}
+*/
